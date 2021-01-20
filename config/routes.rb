79a978d980af467cc62beb88160fdb261c70b185
Indrajeetsinh Chauhan <c_indrajeet@soinnovators.com>
2021-01-20 18:19:38 +0900

    Genarating chatroom channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'messages/create'
-  # root 'application#hello'
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get 'messages/create'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
